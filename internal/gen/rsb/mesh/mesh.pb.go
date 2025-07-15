ab4e68f388697dd48dd7e12d9453e301a99f1385
Miguel Young <myoung@buf.build>
2025-07-15 09:45:30 -0700

    Add missing license attributions to `.proto` files (#2)

--- internal/gen/rsb/mesh/mesh.pb.go
+++ internal/gen/rsb/mesh/mesh.pb.go
@@ -18,6 +18,21 @@
 // 	protoc        (unknown)
 // source: rsb/mesh/mesh.proto
 
+// This schema is taken from the Rust Serialization Benchmark. It is
+// additionally covered by the following license:
+//
+// Copyright 2023-2025 David Koloski
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
 // buf:lint:ignore PACKAGE_VERSION_SUFFIX
 // buf:lint:ignore PACKAGE_DIRECTORY_MATCH
 
